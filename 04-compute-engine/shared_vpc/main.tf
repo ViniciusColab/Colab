fb0345aa9cb00cf50e14aaa65e669d0fe51d24e3
temcarlosrocha <carlos.rocha@ipnet.cloud>
2024-10-09 19:46:00 -0300

    just checking

--- 04-compute-engine/shared_vpc/main.tf
+++ 04-compute-engine/shared_vpc/main.tf
@@ -33,14 +33,20 @@ module "gke" {
   project_id                 = var.project_id
   name                       = "${local.cluster_type}-cluster${var.cluster_name_suffix}"
   region                     = var.region
-  network                    = var.network
+  
   network_project_id         = var.network_project_id
+  network                    = var.network
   subnetwork                 = var.subnetwork
+  
   ip_range_pods              = var.ip_range_pods
   ip_range_services          = var.ip_range_services
+  
   create_service_account     = false
   service_account            = var.compute_engine_service_account
+  
   add_cluster_firewall_rules = true
+  
   firewall_inbound_ports     = ["9443", "15017"]
+
   deletion_protection        = false
 }
